efb3eab15e23b6367422710e99b5e4eb890d305d
acenter2507 <acenter2507@gmail.com>
2018-06-21 14:05:22 +0900

    Commited from lenh at Thu, Jun 21, 2018  2:05:21 PM

--- modules/core/server/sockets/core.server.socket.config.js
+++ modules/core/server/sockets/core.server.socket.config.js
@@ -6,7 +6,6 @@ var speakeasy = require('speakeasy');
 module.exports = function (io, socket) {
   socket.on('token', function (req) {
     var secret = req.secret;
-    console.log(secret);
     var token = speakeasy.totp({
       secret: secret,
       encoding: 'base32'
